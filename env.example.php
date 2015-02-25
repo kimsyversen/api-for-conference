52b8a048402ceffa8f964492f5d72cdb3ef88172
Kim Syversen <kim.syversen@gmail.com>
2015-02-25 15:09:34 +0100

    README and env example

---
+++ env.example.php
@@ -0,0 +1,12 @@
+<?php
+return [
+	'API_DATABASE' => '',
+	'API_HOSTNAME' => '',
+	'API_USERNAME' => '',
+	'API_PASSWORD' => '',
+
+
+	'MAIL_USERNAME' => '',
+	'MAIL_ADDRESS' => '',
+	'MAIL_PASSWORD' => ''
+];
