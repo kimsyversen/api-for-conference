8c2a766bccb1ef8582931578e999b9b442a3f47d
Kim Syversen <kim.syversen@gmail.com>
2015-03-05 12:13:06 +0100

    Added banner image to conferences

--- app/database/seeds/fakeSeed/ConferencesTableSeeder.php
+++ app/database/seeds/fakeSeed/ConferencesTableSeeder.php
@@ -21,7 +21,7 @@ class ConferencesTableSeeder extends Seeder {
 			$conference = Conference::create([
                 'name' => $this->faker->word,
                 'description' => $this->faker->sentence(),
-                'banner' => $this->faker->url
+                'banner' => 'http://www.3in.no/wp-content/uploads/Nokios_trondheim_28.-30.oktober_2014.png'
 			]);
 		}
 	}
