6ddcd475104220c5ce1cfd7a871533599ca8f30b
Kim Syversen <kim.syversen@gmail.com>
2015-03-09 19:23:25 +0100

    One more test

--- tests/integration/IntegrationTest.php
+++ tests/integration/IntegrationTest.php
@@ -1,5 +1,15 @@
-<?php 
+<?php
+use Faker\Factory as Faker;
 class IntegrationTest extends \Codeception\TestCase\Test {
+
+	//TODO: Fjerne?
+	protected $faker;
+
+	function __construct()
+	{
+		$this->faker = Faker::create();
+
+	}
 
 	public function cleanDatabase()
 	{
--- tests/integration/Users/UserRepositoryTest.php
+++ tests/integration/Users/UserRepositoryTest.php
@@ -3,22 +3,36 @@
 
 use Carbon\Carbon;
 use Laracasts\TestDummy\Factory as TestDummy;
+
 use Uninett\Eloquent\Users\Repositories\UserRepository;
 use Uninett\Eloquent\Users\User;
 
 class UserRepositoryTest extends IntegrationTest
 {
+	protected $tester;
+	protected $repository;
+
+	/*
+	 * Om vi importerer Factory tror jeg vi kan gjøre noe sånt som dette:
+	 */
+	public function getStub()
+	{
+		return [
+			'email' => str_random(5) . $this->faker->safeEmail,
+			'password' => 'password',
+			'confirmation_code' => str_random(40),
+			'created_at' => Carbon::now(),
+			'updated_at' => Carbon::now(),
+		];
+	}
+
 	/**
 	 * @var \IntegrationTester
 	 */
-	protected $tester;
-
-	protected $repository;
 
 	protected function _before()
 	{
-		$this->repository = new UserRepository();
-
+		$this->repository = new UserRepository;
 		$this->cleanDatabase();
 	}
 
@@ -27,6 +41,18 @@ class UserRepositoryTest extends IntegrationTest
 
 	}
 
+	/** @test **/
+	public function it_can_create_users()
+	{
+		$numberOfUsersToCreate = 5;
+
+		TestDummy::times($numberOfUsersToCreate)->create('Uninett\Eloquent\Users\User');
+
+		$users = User::all()->count();
+
+		$this->assertEquals($numberOfUsersToCreate, $users);
+
+	}
 	/** @test **/
 	public function it_can_save_a_user()
 	{
@@ -37,6 +63,7 @@ class UserRepositoryTest extends IntegrationTest
 			'created_at' => Carbon::now(),
 			'updated_at' => Carbon::now(),
 		];
+
 
 		$result = User::create($user);
 
@@ -49,12 +76,9 @@ class UserRepositoryTest extends IntegrationTest
 	{
 		$created_user =  TestDummy::create('Uninett\Eloquent\Users\User');
 
-
-
 		$result = $this->repository->verify($created_user->confirmation_code);
 
 		$this->assertInstanceOf('\Uninett\Eloquent\Users\User', $result);
 
 	}
-
 }
