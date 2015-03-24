35a028f4ba52810366d7902c76cdab3c0443d586
Kim Syversen <kim.syversen@gmail.com>
2015-03-24 09:16:30 +0100

    Started on nokiosseed

--- app/database/seeds/fakeSeed/SessionsTableSeeder.php
+++ app/database/seeds/fakeSeed/SessionsTableSeeder.php
@@ -6,32 +6,6 @@ use Uninett\Eloquent\Conferences\Conference;
 use Uninett\Eloquent\Sessions\Session;
 
 class SessionsTableSeeder extends UninettSeeder {
-
-/*	public function run()
-	{
-		$faker = Faker::create();
-
-        $conference_ids = Conference::lists('id');
-
-		foreach(range(1, 50) as $index)
-		{
-			$randomDay = $faker->numberBetween(0,30);
-			$randomHour = $faker->numberBetween(0,24);
-
-			$startDate = Carbon::now()->addDays($randomDay)->addHours($randomHour)->minute(0)->second(0)->toDateTimeString();
-            $endDate = Carbon::now()->addDays($randomDay)->addHours($randomHour)->addHours($faker->numberBetween(1,3))->minute(0)->second(0)->toDateTimeString();
-
-			Session::create([
-                'conference_id' => $faker->randomElement($conference_ids),
-                'title' => $faker->sentence(),
-                'description' => $faker->paragraph() . ' ' . $faker->paragraph() . ' ' . $faker->paragraph() . ' ' . $faker->paragraph(),
-                'location' => $faker->address,
-                'start_time' => $startDate,
-                'end_time' => $endDate,
-			]);
-		}
-	}*/
-
 	public function run()
 	{
 		$conference_ids = Conference::lists('id');
