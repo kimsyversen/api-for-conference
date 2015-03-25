02de7c278f149fc50e1f4fd2d1e63635bf969f06
Kim Syversen <kim.syversen@gmail.com>
2015-03-25 07:36:19 +0100

    Update hostname for environment

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 	'production' => array('shadowcat', 'shadowcat.uninett.no'),
-	'local' =>  array('kImac' , 'administrator-VirtualBox')
+	'local' =>  array('Kimba', 'kImac', 'kImac.local', 'administrator-VirtualBox')
 ));
 
 /*
