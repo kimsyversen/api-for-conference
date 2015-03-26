91784bddc91dd05857db0d60afb88bf20d6cdb84
Kim Syversen <kim.syversen@gmail.com>
2015-03-26 11:38:19 +0100

    Add check spamfolder to email message

--- app/views/emails/registration/verify.blade.php
+++ app/views/emails/registration/verify.blade.php
@@ -7,7 +7,7 @@
 <h2>Activate your account</h2>
 
 <div>
-    <p class="lead">Hi chief! Thanks for registering. You must activate your account to be able to log in.
+    <p class="lead">Hi chief! Thanks for registering. You must activate your account to be able to log in. If you don't see any email, please check your spam folder.
     <a href="{{ $confirmation_url }}">Click here to activate your account</a></p>
 
 
