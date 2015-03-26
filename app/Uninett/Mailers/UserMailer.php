f0a73eb6c2ea2757d6d50dff0becd1edf2e6057f
Kim Syversen <kim.syversen@gmail.com>
2015-03-26 08:33:29 +0100

    Changed subject in activation email

--- app/Uninett/Mailers/UserMailer.php
+++ app/Uninett/Mailers/UserMailer.php
@@ -7,7 +7,7 @@ class UserMailer extends Mailer {
 
     public function sendMessageTo(User $user)
     {
-        $subject =  'Please verify your account';
+        $subject =  'Please activate your account';
         $view = 'emails.registration.verify';
         $data = [
             'confirmation_url' => URL::to('register/verify/' . $user->confirmation_code ),
