7a277624c6bb0633653a5284d140ddfab4ef163d
Kim Syversen <kim.syversen@gmail.com>
2015-04-28 11:12:46 +0200

    Update seed

--- app/database/seeds/laeringsfestivalen/ConferencesTableSeeder.php
+++ app/database/seeds/laeringsfestivalen/ConferencesTableSeeder.php
@@ -16,7 +16,7 @@ class ConferencesTableSeeder extends \Seeder {
             'description' => 'Læringsfestivalen er en nasjonal konferanse for universitets- og høyskolesektoren med fokus på nye metoder for læring og undervisning ved bruk av teknologi. Målet er å inspirere og motivere, og vise gode og spennende tilnærminger til undervisning og læring, og reflektere rundt de teknologiske mulighetene som eksisterer.',
             'banner' =>  'img/LF15_header.jpg',
 			'country' => 'Norway',
-			'city' => 'Trondheim (På Gløshaugen, i Elektrobygget, O. S. Bragstads plass 2)',
+			'city' => 'Trondheim (Gløshaugen, i Elektrobygget, O. S. Bragstads plass 2)',
 			'start_date' => Carbon::create(2015, 05, 04),
             'end_date' => Carbon::create(2015, 05, 05),
 		]);
