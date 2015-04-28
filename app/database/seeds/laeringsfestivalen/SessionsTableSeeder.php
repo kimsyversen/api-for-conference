80bd06a16ea5a051b8f103681407493a733f916e
Kim Syversen <kim.syversen@gmail.com>
2015-04-28 10:43:47 +0200

    Add more seeds

--- app/database/seeds/laeringsfestivalen/SessionsTableSeeder.php
+++ app/database/seeds/laeringsfestivalen/SessionsTableSeeder.php
@@ -20,7 +20,7 @@ class SessionsTableSeeder extends  \Seeder {
 			'title' => 'Registrering i Elektrobygget på Gløshaugen, NTNU',
 			'description' => '',
 			'location' => 'Elektrobygget',
-			'category' => 'other',
+			'category' => 'professional',
 			'target_audience' => 'All',
 			'confirmed' => true,
 			'start_time' => Carbon::create(2015, 05, 04, 9, 00, 00),
@@ -375,10 +375,6 @@ class SessionsTableSeeder extends  \Seeder {
 			'description' => 'Canvas by Instructure is the learning management system that makes teaching and learning easier. Its customisable platform and built-in features provide teachers and students with modern tools and resources that truly integrate with multimedia.',
 		]);
 
-
-
-
-
 		$session_17 = Session::create([
 			'conference_id' => $conferenceId,
 			'title' => 'Middag med Teach-meet',
@@ -400,33 +396,387 @@ class SessionsTableSeeder extends  \Seeder {
 
 
 
-		Session::create([
+
+
+		//Neste dag
+
+
+		$session_18 = Session::create([
 			'conference_id' => $conferenceId,
-			'title' => '',
+			'title' => 'Plenum: Social Media and Co-learning',
+			'description' => 'Vi gjør oppmerksom på at foredraget vil bli holdt på engelsk.',
+			'location' => 'Auditorium EL 5',
+			'category' => 'professional',
+			'target_audience' => 'All',
+			'confirmed' => true,
+			'start_time' => Carbon::create(2015, 05, 05, 9, 00, 01),
+			'end_time' => Carbon::create(2015, 05, 05, 10, 00, 00),
+		]);
+
+
+		Speaker::create([
+			'session_id' => $session_18->id,
+			'first_name' => 'Howard',
+			'last_name' => ' Rheingold',
+			'affiliation' => 'Rheingold University',
+			'title' => 'Regional Director',
+			'description' => 'Rehingold omtales som den første innbyggeren på Internett. Rheingolds spesialområde er hvordan kommunikasjonsteknologi virker inn på samfunnet sosialt, politisk og kulturelt. Han er spesielt interessert i hvordan dette påvirker læring og utdanning, og er en av verdens mest inviterte talere på konferanser om dette temaet. Rheingold har undervist på universitet som Stanford og Berkeley innen sine spesialområder.',
+		]);
+
+
+		$session_19 = Session::create([
+			'conference_id' => $conferenceId,
+			'title' => 'Pause',
 			'description' => '',
 			'location' => '',
-			'category' => '',
-			'target_audience' => '',
+			'category' => 'break',
+			'target_audience' => 'All',
 			'confirmed' => true,
-			'start_time' => Carbon::create(2015, 05, 04, 9, 00, 00),
-			'end_time' => Carbon::create(2015, 05, 04, 10, 00, 01),
+			'start_time' => Carbon::create(2015, 05, 05, 10, 00, 01),
+			'end_time' => Carbon::create(2015, 05, 05, 10, 30, 00),
 		]);
 
 
 
 
 
+		//Spor
 
-		Session::create([
+		$session_20 = Session::create([
 			'conference_id' => $conferenceId,
-			'title' => '',
+			'title' => 'Digital eksamen. Fra behov til gjennomføring',
+			'description' => 'Det medisinske fakultetet, UiO, hadde tidligere vanlige, essaypregete skole-eksamener. I en ekstern evaluering av studiet og eksamen ble det påvist at denne eksamensformen hadde både lav content validity, sampling validity og reliabilitet. Det ble da satt i gang en stor reform av eksamen med innføring blant annet av OSCE-eksamener og digitale eksamener. Digital eksamen ved med.fak. har således et pedagogisk fundament, ikke et logistisk. I foredraget gjøres det rede for denne bakgrunnen, hvordan digital eksamen ble bygd opp for å svare på de eksamenspedagogiske utfordringene, kvalitetssikringstiltak før og etter eksamen, digital sensur og kort om veien videre: nasjonale prøver, gjenbruk, kobling formative/summative prøver, digital evaluering av praktiske prøver (OSCE), håndtering av flerspråklige eksamenssett.',
+			'location' => 'Auditorium EL 5',
+			'category' => 'professional',
+			'target_audience' => 'All',
+			'confirmed' => true,
+			'start_time' => Carbon::create(2015, 05, 05, 10, 30, 01),
+			'end_time' => Carbon::create(2015, 05, 05, 11, 15, 00),
+		]);
+
+		Speaker::create([
+			'session_id' => $session_20->id,
+			'first_name' => 'Per',
+			'last_name' => ' Grøttum',
+			'affiliation' => 'Det medisinske fakultet, Universitetet i Oslo',
+			'title' => 'Professor i medisinsk informatikk ved Seksjon for medisinsk informatikk',
+			'description' => 'Rehingold omtales som den første innbyggeren på Internett. Rheingolds spesialområde er hvordan kommunikasjonsteknologi virker inn på samfunnet sosialt, politisk og kulturelt. Han er spesielt interessert i hvordan dette påvirker læring og utdanning, og er en av verdens mest inviterte talere på konferanser om dette temaet. Rheingold har undervist på universitet som Stanford og Berkeley innen sine spesialområder.',
+		]);
+
+
+
+		$session_21 = Session::create([
+			'conference_id' => $conferenceId,
+			'title' => 'Juridiske aspekter ved digital eksamen',
+			'description' => 'Jegersberg er juridisk rådgiver i IT-direktørens stab på Universitetets senter for informasjonsteknologi ved UiO, med hovedarbeidsområder innenfor IT og personvern. Innledningen hennes tar utgangspunkt i en juridisk rapport som ble skrevet våren 2014 av en arbeidsgruppe med jurister på oppdrag fra Ekspertgruppen for digital vurdering og eksamen. Rapporten belyser og gjennomgår juridiske utfordringer knyttet til gjennomføringen av digital vurdering og eksamen Hele sektoren må forberede seg på en ny hverdag med digitalisering av læremidler, undervisning og vurderingsformer, og dette betyr at vi alle må tilpasse oss nye juridiske problemstillinger. De fleste problemstillingene eksisterer allerede, men digitaliseringen krever at vi tar nye gjennomganger og ser på dem i et annet lys enn tidligere. Stikkord: Bruk av utstyr - eget og institusjonens, administrative spørsmål om blant annet lagring og innsyn, rettigheter, plikter, ansvar, krav til autentisering, monitorering, plagiatkontroll og skytjeneste.',
+			'location' => 'Auditorium EL 5',
+			'category' => 'professional',
+			'target_audience' => 'All',
+			'confirmed' => true,
+			'start_time' => Carbon::create(2015, 05, 05, 10, 30, 01),
+			'end_time' => Carbon::create(2015, 05, 05, 11, 15, 00),
+		]);
+
+		Speaker::create([
+			'session_id' => $session_21->id,
+			'first_name' => 'Maren',
+			'last_name' => ' Jegersberg,',
+			'affiliation' => 'Universitetet i Oslo',
+			'title' => 'Juridisk rådgiver',
+			'description' => '',
+		]);
+
+
+		$session_22 = Session::create([
+			'conference_id' => $conferenceId,
+			'title' => 'Digital tilstand 2014, fra undervisning til læring - et spørsmål om utdanningsledelse?',
+			'description' => 'Presentasjonen vil gi et oversiktsbilde av den digitale tilstanden i UH-sektoren med utgangspunkt i den nasjonale kvantitative undersøkelsen Digital tilstand 2014. Undersøkelsen ser nærmere på en del forhold knyttet til bruk, tilrettelegging for bruk og betingelser for bruk av teknologi i undervisning. Undersøkelsen ble i 2014 gjennomført for 3. gang, og gir dermed et godt grunnlag for å beskrive både tilstanden, utviklingstrekk og utfordringer knyttet til dagens tilstand. Stikkord: strategier og handlingsplaner, virkemidler, holdninger, undervisnings- og læringsaktiviteter, teknologibruk',
+			'location' => 'Auditorium EL 6',
+			'category' => 'professional',
+			'target_audience' => 'All',
+			'confirmed' => true,
+			'start_time' => Carbon::create(2015, 05, 05, 10, 30, 01),
+			'end_time' => Carbon::create(2015, 05, 05, 11, 15, 00),
+		]);
+
+		Speaker::create([
+			'session_id' => $session_22->id,
+			'first_name' => 'Hilde Anite',
+			'last_name' => ' Ørnes,',
+			'affiliation' => 'Norgesuniversitetet',
+			'title' => 'Seniorrådgiver i Norgesuniversitetet og prosjektleder for undersøkelsen Digital tilstand 2014',
+			'description' => '',
+		]);
+
+
+
+
+		$session_23 = Session::create([
+			'conference_id' => $conferenceId,
+			'title' => 'Digitalisering for kvalitet - Hva må gjøres?',
+			'description' => '',
+			'location' => 'Auditorium EL 6',
+			'category' => 'professional',
+			'target_audience' => 'All',
+			'confirmed' => true,
+			'start_time' => Carbon::create(2015, 05, 05, 10, 30, 01),
+			'end_time' => Carbon::create(2015, 05, 05, 11, 15, 00),
+		]);
+
+		Speaker::create([
+			'session_id' => $session_23->id,
+			'first_name' => 'Jon',
+			'last_name' => ' Lanestedt,',
+			'affiliation' => 'Norgesuniversitetet',
+			'title' => 'Seniorrådgiver',
+			'description' => '',
+		]);
+
+
+
+		$session_24 = Session::create([
+			'conference_id' => $conferenceId,
+			'title' => 'Lunsj',
 			'description' => '',
 			'location' => '',
-			'category' => '',
-			'target_audience' => '',
+			'category' => 'social',
+			'target_audience' => 'All',
 			'confirmed' => true,
-			'start_time' => Carbon::create(2015, 05, 04, 9, 00, 00),
-			'end_time' => Carbon::create(2015, 05, 04, 10, 00, 01),
+			'start_time' => Carbon::create(2015, 05, 05, 11, 15, 01),
+			'end_time' => Carbon::create(2015, 05, 05, 12, 00, 00),
+		]);
+
+
+		$session_25 = Session::create([
+			'conference_id' => $conferenceId,
+			'title' => 'Kreativ pedagogikk med teknologi i høyere utdanning',
+			'description' => 'Hva skjer når studentene finner all inspirasjon på nett? Instrumentalundervisningen har allerede opplevd dette paradigmeskiftet. Vi har gått fra å være mesterlærere til å bli akademiske VJ. I min undervisningspraksis leter jeg etter nye muligheter for læring. Hva skjer når vi digitaliserer pensum og egen praksis? Mye undervisningspraksis er basert på behaviorisme og kognitivisme, men hva skjer når det digitale sprenger rammene og åpner for høyeffektiv sosialkonstruktivistisk læring og konnektivisme? Med utgangspunkt i WOWmodellen vil jeg vise hvordan jeg gradvis utvikler eksisterende metoder, jobber for individuell integrering i egen praksis og sammen med studentene jobber videre mot en kollektiv integrering av teknologi i læring.',
+			'location' => 'Auditorium EL 5',
+			'category' => 'professional',
+			'target_audience' => 'All',
+			'confirmed' => true,
+			'start_time' => Carbon::create(2015, 05, 05, 10, 30, 01),
+			'end_time' => Carbon::create(2015, 05, 05, 11, 15, 00),
+		]);
+
+		Speaker::create([
+			'session_id' => $session_25->id,
+			'first_name' => 'Lage',
+			'last_name' => ' Thune Myrberget',
+			'affiliation' => 'Westerdals ACT Oslo',
+			'title' => 'Høyskolelektor',
+			'description' => '',
+		]);
+
+
+		$session_26 = Session::create([
+			'conference_id' => $conferenceId,
+			'title' => 'Nasjonale støttetjenester og verktøykasse',
+			'description' => '',
+			'location' => 'Auditorium EL 6',
+			'category' => 'professional',
+			'target_audience' => 'All',
+			'confirmed' => true,
+			'start_time' => Carbon::create(2015, 05, 05, 12, 00, 01),
+			'end_time' => Carbon::create(2015, 05, 05, 12, 45, 00),
+		]);
+
+		Speaker::create([
+			'session_id' => $session_26->id,
+			'first_name' => 'Frode',
+			'last_name' => ' Arntsen',
+			'affiliation' => 'Bibsys',
+			'title' => 'Direktør',
+			'description' => 'Variert operativ tjeneste i Luftforsvaret, senest fra Luftkrigsskolen der han frem til 2001 var leder for en av fagavdelingene. Fra 2001 - 2003 var han rådgiver i Mercuri Urval med fokus på lederrekruttering, utvelgelse og utviklingsprosesser. Fra 2003 til august 2011 ledet han NTNUs etter- og videreutdanningsvirksomhet (NTNU VIDERE), med tilbud til bedrifter og enkeltpersoner. Startet hos BIBSYS 1. sep 2011.',
+		]);
+
+		Speaker::create([
+			'session_id' => $session_26->id,
+			'first_name' => 'Thorleif',
+			'last_name' => ' Hallén',
+			'affiliation' => 'UNINETT',
+			'title' => 'Seniorrådgiver, Tjenester & leveranser',
+			'description' => '',
+		]);
+
+		Speaker::create([
+			'session_id' => $session_26->id,
+			'first_name' => 'Kjetil',
+			'last_name' => ' Knarlag',
+			'affiliation' => 'NTNU',
+			'title' => 'Prosjektleder',
+			'description' => 'Kjetil er Universells leder. Han har utdanning i pedagogikk og samfunnsvitenskap med hovedfag i politisk historie. Han har i tillegg etterutdanning i universell utforming ved UMB og NTNU. Han har arbeidet 6 år som rådgiver for studenter med funksjonsnedsettelser ved NTNU, og har etablert og utviklet pådriverenheten fra starten i 2003. Han er en mye brukt foredragsholder innen temaene inkludering og universell utforming av læringsmiljø.',
+		]);
+
+
+		$session_27 = Session::create([
+			'conference_id' => $conferenceId,
+			'title' => 'Pause',
+			'description' => '',
+			'location' => '',
+			'category' => 'break',
+			'target_audience' => 'All',
+			'confirmed' => true,
+			'start_time' => Carbon::create(2015, 05, 05, 12, 40, 01),
+			'end_time' => Carbon::create(2015, 05, 05, 13, 00, 00),
+		]);
+
+
+
+		$session_28 = Session::create([
+			'conference_id' => $conferenceId,
+			'title' => 'Ideelt digitalt læringsmiljø',
+			'description' => '',
+			'location' => 'Auditorium EL 5',
+			'category' => 'professional',
+			'target_audience' => 'All',
+			'confirmed' => true,
+			'start_time' => Carbon::create(2015, 05, 05, 13, 00, 01),
+			'end_time' => Carbon::create(2015, 05, 05, 13, 45, 00),
+		]);
+
+		Speaker::create([
+			'session_id' => $session_28->id,
+			'first_name' => 'Magnus',
+			'last_name' => ' Aarskaug Rud',
+			'affiliation' => 'Altund',
+			'title' => '',
+			'description' => 'Magnus Aarskaug Rud studerer industriell matematikk ved Institutt for matematiske fag (IMF). Magnus tar for seg kvaliteten på øvingsopplegget ved utvalgte emner. E-post: magnurud@stud.ntnu.no  Mobil: 975 98 733',
+		]);
+
+
+
+
+
+		$session_29 = Session::create([
+			'conference_id' => $conferenceId,
+			'title' => 'Eksisterer den "digitale" studenten? Om studenters prioriteringer og personlige ressurser',
+			'description' => 'Studenters bruk av tid og innsats i studiene har blitt diskutert både i forskning og i massemediene. I de fleste publiseringene har fokuset vært på å gi lærere anbefalinger om hvordan de kan bruke teknologi for å forbedre studenters "tilnærming til læring". En fersk studie fra 2014 av studenters arbeidsmåter og ressursbruk har derimot vist at digitale verktøy spiller kun en underordnet rolle i studenters hverdag, og at det er optimaliseringen av personlige ressurser som står i fokus for studentene. Det kan altså spørres om den "digitale" studenten virkelig eksisterer og hva man faktisk klarer å oppnå med digitaliseringen av undervisningen. Stikkord for tema/problemstillinger: Personlige ressurser, optimalisering, digital student, teknologi i undervisning',
+			'location' => 'Auditorium EL 5',
+			'category' => 'professional',
+			'target_audience' => 'All',
+			'confirmed' => true,
+			'start_time' => Carbon::create(2015, 05, 05, 13, 00, 01),
+			'end_time' => Carbon::create(2015, 05, 05, 13, 45, 00),
+		]);
+
+		Speaker::create([
+			'session_id' => $session_29->id,
+			'first_name' => 'Katja',
+			'last_name' => 'Hakel',
+			'affiliation' => 'NTNU',
+			'title' => 'Universitetslektor ved Program for lærerutdanning',
+			'description' => '',
+		]);
+
+
+
+
+
+		$session_30 = Session::create([
+			'conference_id' => $conferenceId,
+			'title' => 'Opphavsrett - erfaringer som viser utfordringer – Faglærerperspektiv',
+			'description' => '',
+			'location' => 'Auditorium EL 6',
+			'category' => 'professional',
+			'target_audience' => 'All',
+			'confirmed' => true,
+			'start_time' => Carbon::create(2015, 05, 05, 13, 00, 01),
+			'end_time' => Carbon::create(2015, 05, 05, 13, 45, 00),
+		]);
+
+		Speaker::create([
+			'session_id' => $session_30->id,
+			'first_name' => 'Magnus H.',
+			'last_name' => 'Sandberg',
+			'affiliation' => 'NTNU, Institutt for sosiologi og statsvitenskap',
+			'title' => 'Universitetslektor',
+			'description' => '',
+		]);
+
+
+
+
+		$session_31 = Session::create([
+			'conference_id' => $conferenceId,
+			'title' => 'DelRett hjelper deg – opphavsrett og undervisning',
+			'description' => 'DelRett tilbyr en samlende tjeneste for opphavsrettslige spørsmål rettet mot grunnopplæringen og høyere utdanning, med det formål å stimulere til økt bruk av digitale læringsressurser. Dette gjør vi ved å besvare typiske spørsmål om opphavsrett og undervisning. Lurer du på om du kan bruke Spotify i undervisningen eller legge ut opptak av forelesninger i læringsplattformen? Du finner svar på DelRett.no Stikkord: Opphavsrett. Undervisning. Deling. Gjenbruk. Klarering.',
+			'location' => 'Auditorium EL 6',
+			'category' => 'professional',
+			'target_audience' => 'All',
+			'confirmed' => true,
+			'start_time' => Carbon::create(2015, 05, 05, 13, 00, 01),
+			'end_time' => Carbon::create(2015, 05, 05, 13, 45, 00),
+		]);
+
+		Speaker::create([
+			'session_id' => $session_31->id,
+			'first_name' => 'Hilde S.',
+			'last_name' => 'Gaard',
+			'affiliation' => 'Norgesuniversitet',
+			'title' => 'Rådgiver',
+			'description' => '',
+		]);
+
+
+		$session_32 = Session::create([
+			'conference_id' => $conferenceId,
+			'title' => 'Pause',
+			'description' => '',
+			'location' => '',
+			'category' => 'break',
+			'target_audience' => 'All',
+			'confirmed' => true,
+			'start_time' => Carbon::create(2015, 05, 05, 13, 45, 01),
+			'end_time' => Carbon::create(2015, 05, 05, 14, 00, 00),
+		]);
+
+
+
+		$session_33 = Session::create([
+			'conference_id' => $conferenceId,
+			'title' => 'Plenum: The Power of Personalized Learning in Higher Education',
+			'description' => 'Keynote description: In a digital world, it becomes possible to measure data to discover exactly how lessons, apps, or games really help drive student understanding. Soon, education companies that create the tools and textbooks students use will have to prove how much they actually improve learning. This means publishers will create ever more effective materials to compete in the marketplace, students will get better outside-of class materials, and professors will have ever more insight into student progress. Jose Ferreira, founder and CEO of leading adaptive company Knewton, will explain the impact of the shift from print to digital learning materials in higher education.',
+			'location' => 'Auditorium EL 5',
+			'category' => 'professional',
+			'target_audience' => 'All',
+			'confirmed' => true,
+			'start_time' => Carbon::create(2015, 05, 05, 14, 00, 01),
+			'end_time' => Carbon::create(2015, 05, 05, 14, 45, 00),
+		]);
+
+		Speaker::create([
+			'session_id' => $session_33->id,
+			'first_name' => 'Jose',
+			'last_name' => 'Ferreira',
+			'affiliation' => 'Knewton',
+			'title' => 'Administrerende direktør',
+			'description' => '',
+		]);
+
+
+
+
+
+		$session_34 = Session::create([
+			'conference_id' => $conferenceId,
+			'title' => 'Avslutning',
+			'description' => '',
+			'location' => 'Auditorium EL 5',
+			'category' => 'professional',
+			'target_audience' => 'All',
+			'confirmed' => true,
+			'start_time' => Carbon::create(2015, 05, 05, 14, 00, 01),
+			'end_time' => Carbon::create(2015, 05, 05, 14, 45, 00),
+		]);
+
+		Speaker::create([
+			'session_id' => $session_34->id,
+			'first_name' => 'Berit J.',
+			'last_name' => 'Kjelstad',
+			'affiliation' => 'NTNU',
+			'title' => 'Prorektor',
+			'description' => 'Berit Kjeldstad er Prorektor for utdanning og læringskvalitet ved NTNU. Kjeldstad ble førsteamanuensis ved Fysisk institutt i 1992 innen fagområdet energi- og miljøfysikk, og ble professor i 2001. I januar 2006 ble Kjeldstad instituttleder ved Institutt for fysikk. Siden 1993 har hun arbeidet med problemstillinger knyttet til UV-stråling fra sola, atmosfærefysikk og klima, og har bl.a. 10 års erfaring fra Norges forskningsråd innen klimaprogrammet. Kjelstad ble prorektor for utdanning i 2009.',
 		]);
 	}
 }
