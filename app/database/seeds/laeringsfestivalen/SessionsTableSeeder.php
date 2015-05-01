12000d36288cfeb46a5df53cf01cf1d8f322d28f
Kim Syversen <kim.syversen@gmail.com>
2015-05-01 20:32:24 +0200

    Updated seeds

--- app/database/seeds/laeringsfestivalen/SessionsTableSeeder.php
+++ app/database/seeds/laeringsfestivalen/SessionsTableSeeder.php
@@ -28,8 +28,8 @@ class SessionsTableSeeder extends  \Seeder {
 
 		Session::create([
 			'conference_id' => $conferenceId,
-			'title' => 'Strindens promenadeorkester',
-			'description' => 'Velkommen v/ prorektor for utdanning: Berit J. Kjelstad, NTNU',
+			'title' => 'Velkommen v/ prorektor for utdanning: Berit J. Kjelstad, NTNU',
+			'description' => '',
 			'location' => 'Auditorium EL 5',
 			'category' => 'social',
 			'confirmed' => true,
@@ -160,8 +160,8 @@ class SessionsTableSeeder extends  \Seeder {
 
 		$session_7 = Session::create([
 			'conference_id' => $conferenceId,
-			'title' => 'SPOR 2: Digital undervisning og omvendt klasserom',
-			'description' => 'Erfaringer med problembasert læring og bruk av digitale læringsverktøy i store emner med mye teori med praktisk retting, kan ofte være vanskelig å forholde seg til uten at man selv benytter seg av denne i praksis. I emnet TDT4140 Programvareutvikling ved IDI, NTNU, ble alle forelesninger videofilmet våren 2014. Tilbakemeldinger om forelesningene og noe dårlig oppmøte på disse, gjorde at det våren 2015 ble gjort store omlegginger i hvordan emnet ble undervist. Emnet har i vår vært bortimot forelesningsfri, og har i stedet hatt gruppe- og problembaserte case/øvingsoppgaver i en form av omvendt klasserom, dvs. en mye mer aktiv læring. Emnet har ca 320 studenter og foredraget vil i kortform presentere noen erfaringer fra bruk av teknologi i forberedelser og gjennomføring, gjennomføringen av omvendt klasserom med tilbakemeldinger fra studenter og læringsassistenter, hvilke utfordringer som kom, og hva som bør forbedres. Carl-Fredrik Sørensen har våren 2015, hatt hovedansvaret for to emner i 4. og 8. semester med henholdsvis 320 og 70 studenter. Stikkord: Omvendt klasserom, problembasert/gruppebasert/aktiv læring, bruk av videoer, store emners pedagogikk',
+			'title' => 'SPOR 2: (1) Digital undervisning og omvendt klasserom, (2) Innovativ Utdanning ved NTNU',
+			'description' => '(1): Erfaringer med problembasert læring og bruk av digitale læringsverktøy i store emner med mye teori med praktisk retting, kan ofte være vanskelig å forholde seg til uten at man selv benytter seg av denne i praksis. I emnet TDT4140 Programvareutvikling ved IDI, NTNU, ble alle forelesninger videofilmet våren 2014. Tilbakemeldinger om forelesningene og noe dårlig oppmøte på disse, gjorde at det våren 2015 ble gjort store omlegginger i hvordan emnet ble undervist. Emnet har i vår vært bortimot forelesningsfri, og har i stedet hatt gruppe- og problembaserte case/øvingsoppgaver i en form av omvendt klasserom, dvs. en mye mer aktiv læring. Emnet har ca 320 studenter og foredraget vil i kortform presentere noen erfaringer fra bruk av teknologi i forberedelser og gjennomføring, gjennomføringen av omvendt klasserom med tilbakemeldinger fra studenter og læringsassistenter, hvilke utfordringer som kom, og hva som bør forbedres. Carl-Fredrik Sørensen har våren 2015, hatt hovedansvaret for to emner i 4. og 8. semester med henholdsvis 320 og 70 studenter. Stikkord: Omvendt klasserom, problembasert/gruppebasert/aktiv læring, bruk av videoer, store emners pedagogikk. (2): Hva gjøres ved NTNU i 2015 for å tilrettelegge innovative, digitale tjenester for studenter og faglærere? Litt hva, hvorfor og hvordan, på ca 1200 sekunder.',
 			'location' => 'Auditorium EL 6',
 			'category' => 'professional',
 			'confirmed' => true,
@@ -178,20 +178,8 @@ class SessionsTableSeeder extends  \Seeder {
 			'description' => '',
 		]);
 
-
-		$session_8 = Session::create([
-			'conference_id' => $conferenceId,
-			'title' => 'SPOR 2: Innovativ Utdanning ved NTNU',
-			'description' => 'Hva gjøres ved NTNU i 2015 for å tilrettelegge innovative, digitale tjenester for studenter og faglærere? Litt hva, hvorfor og hvordan, på ca 1200 sekunder.',
-			'location' => 'Auditorium EL 6',
-			'category' => 'professional',
-			'confirmed' => true,
-			'start_time' => Carbon::create(2015, 05, 04, 13, 15, 01),
-			'end_time' => Carbon::create(2015, 05, 04, 14, 00, 00),
-		]);
-
 		Speaker::create([
-			'session_id' => $session_8->id,
+			'session_id' => $session_7->id,
 			'first_name' => 'Tore',
 			'last_name' => ' Indreråk',
 			'affiliation' => 'NTNU',
@@ -239,8 +227,8 @@ class SessionsTableSeeder extends  \Seeder {
 
 		$session_11 = Session::create([
 			'conference_id' => $conferenceId,
-			'title' => 'SPOR 2: Organisering av støttetjenester – spiller det noen rolle?',
-			'description' => 'Debatten om organisering av støttetjenester har vært med oss lenge. Over tid har vi sett mange ulike modeller. Spiller det egentlig noen rolle hvordan disse tjenestene er organisert? Og hva betyr organiseringen for de tjenester som kan tilbys? Med utgangspunkt i organiseringen av støttetjenester ved UiT – Norges arktiske universitet - prøver vi å belyse disse spørsmålene. Stikkord: støttetjenester – organiseringsmodeller – støttetjenestens arbeidsfelt',
+			'title' => 'SPOR 2: (1) Organisering av støttetjenester – spiller det noen rolle?, (2) Endringskompetanse for bedre læringsstøtte',
+			'description' => '(1): Debatten om organisering av støttetjenester har vært med oss lenge. Over tid har vi sett mange ulike modeller. Spiller det egentlig noen rolle hvordan disse tjenestene er organisert? Og hva betyr organiseringen for de tjenester som kan tilbys? Med utgangspunkt i organiseringen av støttetjenester ved UiT – Norges arktiske universitet - prøver vi å belyse disse spørsmålene. Stikkord: støttetjenester – organiseringsmodeller – støttetjenestens arbeidsfelt',
 			'location' => 'Auditorium EL 6',
 			'category' => 'professional',
 			'confirmed' => true,
@@ -257,21 +245,8 @@ class SessionsTableSeeder extends  \Seeder {
 			'description' => '',
 		]);
 
-
-
-		$session_12 = Session::create([
-			'conference_id' => $conferenceId,
-			'title' => 'SPOR 2: Endringskompetanse for bedre læringsstøtte',
-			'description' => '',
-			'location' => 'Auditorium EL 6',
-			'category' => 'professional',
-			'confirmed' => true,
-			'start_time' => Carbon::create(2015, 05, 04, 14, 15, 01),
-			'end_time' => Carbon::create(2015, 05, 04, 15, 00, 00),
-		]);
-
 		Speaker::create([
-			'session_id' => $session_12->id,
+			'session_id' => $session_11->id,
 			'first_name' => 'Inge',
 			'last_name' => ' Fottland',
 			'affiliation' => 'NTNU',
@@ -432,8 +407,8 @@ class SessionsTableSeeder extends  \Seeder {
 
 		$session_20 = Session::create([
 			'conference_id' => $conferenceId,
-			'title' => 'SPOR 1: Digital eksamen. Fra behov til gjennomføring',
-			'description' => 'Det medisinske fakultetet, UiO, hadde tidligere vanlige, essaypregete skole-eksamener. I en ekstern evaluering av studiet og eksamen ble det påvist at denne eksamensformen hadde både lav content validity, sampling validity og reliabilitet. Det ble da satt i gang en stor reform av eksamen med innføring blant annet av OSCE-eksamener og digitale eksamener. Digital eksamen ved med.fak. har således et pedagogisk fundament, ikke et logistisk. I foredraget gjøres det rede for denne bakgrunnen, hvordan digital eksamen ble bygd opp for å svare på de eksamenspedagogiske utfordringene, kvalitetssikringstiltak før og etter eksamen, digital sensur og kort om veien videre: nasjonale prøver, gjenbruk, kobling formative/summative prøver, digital evaluering av praktiske prøver (OSCE), håndtering av flerspråklige eksamenssett.',
+			'title' => 'SPOR 1: (1) Digital eksamen. Fra behov til gjennomføring, (2) Juridiske aspekter ved digital eksamen',
+			'description' => '(1): Det medisinske fakultetet, UiO, hadde tidligere vanlige, essaypregete skole-eksamener. I en ekstern evaluering av studiet og eksamen ble det påvist at denne eksamensformen hadde både lav content validity, sampling validity og reliabilitet. Det ble da satt i gang en stor reform av eksamen med innføring blant annet av OSCE-eksamener og digitale eksamener. Digital eksamen ved med.fak. har således et pedagogisk fundament, ikke et logistisk. I foredraget gjøres det rede for denne bakgrunnen, hvordan digital eksamen ble bygd opp for å svare på de eksamenspedagogiske utfordringene, kvalitetssikringstiltak før og etter eksamen, digital sensur og kort om veien videre: nasjonale prøver, gjenbruk, kobling formative/summative prøver, digital evaluering av praktiske prøver (OSCE), håndtering av flerspråklige eksamenssett. (2): Jegersberg er juridisk rådgiver i IT-direktørens stab på Universitetets senter for informasjonsteknologi ved UiO, med hovedarbeidsområder innenfor IT og personvern. Innledningen hennes tar utgangspunkt i en juridisk rapport som ble skrevet våren 2014 av en arbeidsgruppe med jurister på oppdrag fra Ekspertgruppen for digital vurdering og eksamen. Rapporten belyser og gjennomgår juridiske utfordringer knyttet til gjennomføringen av digital vurdering og eksamen Hele sektoren må forberede seg på en ny hverdag med digitalisering av læremidler, undervisning og vurderingsformer, og dette betyr at vi alle må tilpasse oss nye juridiske problemstillinger. De fleste problemstillingene eksisterer allerede, men digitaliseringen krever at vi tar nye gjennomganger og ser på dem i et annet lys enn tidligere. Stikkord: Bruk av utstyr - eget og institusjonens, administrative spørsmål om blant annet lagring og innsyn, rettigheter, plikter, ansvar, krav til autentisering, monitorering, plagiatkontroll og skytjeneste.',
 			'location' => 'Auditorium EL 5',
 			'category' => 'professional',
 			'confirmed' => true,
@@ -452,19 +427,8 @@ class SessionsTableSeeder extends  \Seeder {
 
 
 
-		$session_21 = Session::create([
-			'conference_id' => $conferenceId,
-			'title' => 'SPOR 1: Juridiske aspekter ved digital eksamen',
-			'description' => 'Jegersberg er juridisk rådgiver i IT-direktørens stab på Universitetets senter for informasjonsteknologi ved UiO, med hovedarbeidsområder innenfor IT og personvern. Innledningen hennes tar utgangspunkt i en juridisk rapport som ble skrevet våren 2014 av en arbeidsgruppe med jurister på oppdrag fra Ekspertgruppen for digital vurdering og eksamen. Rapporten belyser og gjennomgår juridiske utfordringer knyttet til gjennomføringen av digital vurdering og eksamen Hele sektoren må forberede seg på en ny hverdag med digitalisering av læremidler, undervisning og vurderingsformer, og dette betyr at vi alle må tilpasse oss nye juridiske problemstillinger. De fleste problemstillingene eksisterer allerede, men digitaliseringen krever at vi tar nye gjennomganger og ser på dem i et annet lys enn tidligere. Stikkord: Bruk av utstyr - eget og institusjonens, administrative spørsmål om blant annet lagring og innsyn, rettigheter, plikter, ansvar, krav til autentisering, monitorering, plagiatkontroll og skytjeneste.',
-			'location' => 'Auditorium EL 5',
-			'category' => 'professional',
-			'confirmed' => true,
-			'start_time' => Carbon::create(2015, 05, 05, 10, 30, 01),
-			'end_time' => Carbon::create(2015, 05, 05, 11, 15, 00),
-		]);
-
 		Speaker::create([
-			'session_id' => $session_21->id,
+			'session_id' => $session_20->id,
 			'first_name' => 'Maren',
 			'last_name' => ' Jegersberg,',
 			'affiliation' => 'Universitetet i Oslo',
@@ -475,8 +439,8 @@ class SessionsTableSeeder extends  \Seeder {
 
 		$session_22 = Session::create([
 			'conference_id' => $conferenceId,
-			'title' => 'SPOR 2: Digital tilstand 2014, fra undervisning til læring - et spørsmål om utdanningsledelse?',
-			'description' => 'Presentasjonen vil gi et oversiktsbilde av den digitale tilstanden i UH-sektoren med utgangspunkt i den nasjonale kvantitative undersøkelsen Digital tilstand 2014. Undersøkelsen ser nærmere på en del forhold knyttet til bruk, tilrettelegging for bruk og betingelser for bruk av teknologi i undervisning. Undersøkelsen ble i 2014 gjennomført for 3. gang, og gir dermed et godt grunnlag for å beskrive både tilstanden, utviklingstrekk og utfordringer knyttet til dagens tilstand. Stikkord: strategier og handlingsplaner, virkemidler, holdninger, undervisnings- og læringsaktiviteter, teknologibruk',
+			'title' => 'SPOR 2: (1) Digital tilstand 2014, fra undervisning til læring - et spørsmål om utdanningsledelse?, (2) Digitalisering for kvalitet - Hva må gjøres?',
+			'description' => '(1): Presentasjonen vil gi et oversiktsbilde av den digitale tilstanden i UH-sektoren med utgangspunkt i den nasjonale kvantitative undersøkelsen Digital tilstand 2014. Undersøkelsen ser nærmere på en del forhold knyttet til bruk, tilrettelegging for bruk og betingelser for bruk av teknologi i undervisning. Undersøkelsen ble i 2014 gjennomført for 3. gang, og gir dermed et godt grunnlag for å beskrive både tilstanden, utviklingstrekk og utfordringer knyttet til dagens tilstand. Stikkord: strategier og handlingsplaner, virkemidler, holdninger, undervisnings- og læringsaktiviteter, teknologibruk',
 			'location' => 'Auditorium EL 6',
 			'category' => 'professional',
 			'confirmed' => true,
@@ -493,22 +457,8 @@ class SessionsTableSeeder extends  \Seeder {
 			'description' => '',
 		]);
 
-
-
-
-		$session_23 = Session::create([
-			'conference_id' => $conferenceId,
-			'title' => 'SPOR 2: Digitalisering for kvalitet - Hva må gjøres?',
-			'description' => '',
-			'location' => 'Auditorium EL 6',
-			'category' => 'professional',
-			'confirmed' => true,
-			'start_time' => Carbon::create(2015, 05, 05, 10, 30, 01),
-			'end_time' => Carbon::create(2015, 05, 05, 11, 15, 00),
-		]);
-
 		Speaker::create([
-			'session_id' => $session_23->id,
+			'session_id' => $session_22->id,
 			'first_name' => 'Jon',
 			'last_name' => ' Lanestedt,',
 			'affiliation' => 'Norgesuniversitetet',
@@ -605,8 +555,8 @@ class SessionsTableSeeder extends  \Seeder {
 
 		$session_28 = Session::create([
 			'conference_id' => $conferenceId,
-			'title' => 'SPOR 1: Ideelt digitalt læringsmiljø',
-			'description' => '',
+			'title' => 'SPOR 1: (1) Ideelt digitalt læringsmiljø, (2) Eksisterer den "digitale" studenten? Om studenters prioriteringer og personlige ressurser',
+			'description' => '(2): Studenters bruk av tid og innsats i studiene har blitt diskutert både i forskning og i massemediene. I de fleste publiseringene har fokuset vært på å gi lærere anbefalinger om hvordan de kan bruke teknologi for å forbedre studenters "tilnærming til læring". En fersk studie fra 2014 av studenters arbeidsmåter og ressursbruk har derimot vist at digitale verktøy spiller kun en underordnet rolle i studenters hverdag, og at det er optimaliseringen av personlige ressurser som står i fokus for studentene. Det kan altså spørres om den "digitale" studenten virkelig eksisterer og hva man faktisk klarer å oppnå med digitaliseringen av undervisningen. Stikkord for tema/problemstillinger: Personlige ressurser, optimalisering, digital student, teknologi i undervisning',
 			'location' => 'Auditorium EL 5',
 			'category' => 'professional',
 			'confirmed' => true,
@@ -624,19 +574,8 @@ class SessionsTableSeeder extends  \Seeder {
 		]);
 
 
-		$session_29 = Session::create([
-			'conference_id' => $conferenceId,
-			'title' => 'SPOR 1: Eksisterer den "digitale" studenten? Om studenters prioriteringer og personlige ressurser',
-			'description' => 'Studenters bruk av tid og innsats i studiene har blitt diskutert både i forskning og i massemediene. I de fleste publiseringene har fokuset vært på å gi lærere anbefalinger om hvordan de kan bruke teknologi for å forbedre studenters "tilnærming til læring". En fersk studie fra 2014 av studenters arbeidsmåter og ressursbruk har derimot vist at digitale verktøy spiller kun en underordnet rolle i studenters hverdag, og at det er optimaliseringen av personlige ressurser som står i fokus for studentene. Det kan altså spørres om den "digitale" studenten virkelig eksisterer og hva man faktisk klarer å oppnå med digitaliseringen av undervisningen. Stikkord for tema/problemstillinger: Personlige ressurser, optimalisering, digital student, teknologi i undervisning',
-			'location' => 'Auditorium EL 5',
-			'category' => 'professional',
-			'confirmed' => true,
-			'start_time' => Carbon::create(2015, 05, 05, 13, 00, 01),
-			'end_time' => Carbon::create(2015, 05, 05, 13, 45, 00),
-		]);
-
 		Speaker::create([
-			'session_id' => $session_29->id,
+			'session_id' => $session_28->id,
 			'first_name' => 'Katja',
 			'last_name' => 'Hakel',
 			'affiliation' => 'NTNU',
@@ -650,8 +589,8 @@ class SessionsTableSeeder extends  \Seeder {
 
 		$session_30 = Session::create([
 			'conference_id' => $conferenceId,
-			'title' => 'SPOR 2: Opphavsrett - erfaringer som viser utfordringer – Faglærerperspektiv',
-			'description' => '',
+			'title' => 'SPOR 2: (1) Opphavsrett - erfaringer som viser utfordringer – Faglærerperspektiv, (2) DelRett hjelper deg – opphavsrett og undervisning',
+			'description' => '(2): DelRett tilbyr en samlende tjeneste for opphavsrettslige spørsmål rettet mot grunnopplæringen og høyere utdanning, med det formål å stimulere til økt bruk av digitale læringsressurser. Dette gjør vi ved å besvare typiske spørsmål om opphavsrett og undervisning. Lurer du på om du kan bruke Spotify i undervisningen eller legge ut opptak av forelesninger i læringsplattformen? Du finner svar på DelRett.no Stikkord: Opphavsrett. Undervisning. Deling. Gjenbruk. Klarering.',
 			'location' => 'Auditorium EL 6',
 			'category' => 'professional',
 			'confirmed' => true,
@@ -668,22 +607,8 @@ class SessionsTableSeeder extends  \Seeder {
 			'description' => '',
 		]);
 
-
-
-
-		$session_31 = Session::create([
-			'conference_id' => $conferenceId,
-			'title' => 'SPOR 2: DelRett hjelper deg – opphavsrett og undervisning',
-			'description' => 'DelRett tilbyr en samlende tjeneste for opphavsrettslige spørsmål rettet mot grunnopplæringen og høyere utdanning, med det formål å stimulere til økt bruk av digitale læringsressurser. Dette gjør vi ved å besvare typiske spørsmål om opphavsrett og undervisning. Lurer du på om du kan bruke Spotify i undervisningen eller legge ut opptak av forelesninger i læringsplattformen? Du finner svar på DelRett.no Stikkord: Opphavsrett. Undervisning. Deling. Gjenbruk. Klarering.',
-			'location' => 'Auditorium EL 6',
-			'category' => 'professional',
-			'confirmed' => true,
-			'start_time' => Carbon::create(2015, 05, 05, 13, 00, 01),
-			'end_time' => Carbon::create(2015, 05, 05, 13, 45, 00),
-		]);
-
 		Speaker::create([
-			'session_id' => $session_31->id,
+			'session_id' => $session_30->id,
 			'first_name' => 'Hilde S.',
 			'last_name' => 'Gaard',
 			'affiliation' => 'Norgesuniversitet',
@@ -728,7 +653,7 @@ class SessionsTableSeeder extends  \Seeder {
 
 		$session_34 = Session::create([
 			'conference_id' => $conferenceId,
-			'title' => 'Avslutning',
+			'title' => 'Avslutning v/ prorektor for utdanning Berit J. Kjelstad, NTNU',
 			'description' => '',
 			'location' => 'Auditorium EL 5',
 			'category' => 'professional',
