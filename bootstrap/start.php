de66363ae297d5b750c8da013bcbc6bf50b1cb26
Magnus Sandgren <magnus.sandgren91@gmail.com>
2015-05-03 15:54:44 +0200

    Added new local environment

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 	'production' => array('shadowcat', 'shadowcat.uninett.no'),
-	'local' =>  array('Kimba', 'kImac', 'kImac.local', 'administrator-VirtualBox', 'homestead')
+	'local' =>  array('Kimba', 'kImac', 'kImac.local', 'administrator-VirtualBox', 'homestead', 'Magnuss-MBP')
 ));
 
 /*
